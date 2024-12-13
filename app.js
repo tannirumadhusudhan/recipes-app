3c268ea716923d81a55712ba47c977c88905caff
tannirumadhusudhan <tannirumadhusudhan@gmail.com>
2024-12-13 18:00:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/api/recipes', require('./routes/recipeRoutes'));
 
-// Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
