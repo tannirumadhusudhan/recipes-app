bddb2786241c3d4e52586d8332f541b549659b0a
tannirumadhusudhan <tannirumadhusudhan@gmail.com>
2024-12-13 22:02:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
