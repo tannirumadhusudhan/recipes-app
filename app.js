b6a570e7d5c0b0ead361ddba1253f1a89430d2e9
tannirumadhusudhan <tannirumadhusudhan@gmail.com>
2024-12-13 21:57:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,5 +14,10 @@ app.use(express.json());
 // Routes
 app.use('/api/recipes', require('./routes/recipeRoutes'));
 
-// Start Server
+app.get('/', (req, res) => {
+    res.send('Welcome to the Recipes API. Use /api/recipes to interact with the API.');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
